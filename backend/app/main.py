7d116884afbf8a79fa774021acd827c3b215ed75
abhiiijith <abhijithpm021@gmail.com>
2026-05-28 13:29:54 +0530

    Added image upload and static file serving

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,17 @@ from app.database.db import engine, Base
 from app.models import user_model
 from app.routes import complaint
 from app.models import complaint_model
+from fastapi.staticfiles import StaticFiles
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.mount(
+    "/uploads",
+    StaticFiles(directory="uploads"),
+    name="uploads"
+)
 
 app.include_router(auth.router)
 app.include_router(complaint.router)
